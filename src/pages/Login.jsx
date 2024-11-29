65580892157d157c3758c95c6480cefee89bf15d
Tatiana Campos <tatiana.campos03@inacapmail.cl>
2024-11-29 09:44:04 -0300

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+// pages/Login.jsx
+import LoginForm from '../components/LoginForm';
+
+function Login() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-2xl">
+        <LoginForm />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
